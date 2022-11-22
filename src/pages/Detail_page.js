01ea1ae5f6e721e1582567281b62f20963fc6bae
Akhancode <akhan.code@gmail.com>
2022-11-22 12:18:09 +0400

    width responsive font

--- src/pages/Detail_page.js
+++ src/pages/Detail_page.js
@@ -31,33 +31,33 @@ const Detail_page = () => {
 
         if(on){
             
-            ws.onmessage = (event)=>{
+           ws.onmessage = async(event)=>{
                 console.log("loading Trade data")
                 
         console.log(event.data)
-        setusdtData(JSON.parse(event.data))
+        await setusdtData(JSON.parse(event.data))
 
   }
 
-    ws_book.onmessage = (event)=>{
+    ws_book.onmessage = async (event)=>{
       
       console.log("loading book ticker")
       console.log(brake)
       
       console.log(event.data)
-      setBook(JSON.parse(event.data))
+      await setBook(JSON.parse(event.data))
       
     }
-    ws_Btc.onmessage = (event)=>{
+    ws_Btc.onmessage = async (event)=>{
         console.log("loading Trade data")               
         console.log(event.data)
-        setbtcData(JSON.parse(event.data))
+        await setbtcData(JSON.parse(event.data))
 
 }
-ws_pair_usdt.onmessage = (event)=>{
+ws_pair_usdt.onmessage = async (event)=>{
 console.log("loading Trade data")               
 console.log(event.data)
-setPairUsdtData(JSON.parse(event.data))
+await setPairUsdtData(JSON.parse(event.data))
 
 }
     
@@ -118,9 +118,9 @@ setPairUsdtData(JSON.parse(event.data))
    
   return (
     <>
-    <nav style={{}}>
+    <div className='navDiv'>
         {/* BUTTON WITH DIV */}
-            <button style={{  all: "unset",padding:"20px", cursor: "pointer"}}onClick={()=>{
+            <button style={{  all: "unset", cursor: "pointer"}}onClick={()=>{
                 console.log(ws.readyState)
                 console.log(ws.OPEN)
                 console.log("clicked")}}>
@@ -128,7 +128,7 @@ setPairUsdtData(JSON.parse(event.data))
                         <Link to="/">Home</Link></div>
             </button>
         {/* BUTTON WITH DIV */}
-            <button style={{  all: "unset",padding:"20px", cursor: "pointer"}}onClick={()=>{
+            <button style={{  all: "unset", cursor: "pointer"}}onClick={()=>{
                 console.log(on)
                 // change on 
                 on?setOn(false):setOn(true)
@@ -138,7 +138,7 @@ setPairUsdtData(JSON.parse(event.data))
                     <div style={{backgroundColor:"grey",padding:"10px"}}>{on?`DISCONNECT`:`CONNECT`}</div>
             </button>
         {/* BUTTON WITH DIV */}
-            <button style={{  all: "unset",padding:"20px", cursor: "pointer"}}onClick={()=>{
+            <button style={{  all: "unset", cursor: "pointer"}}onClick={()=>{
                
                 // ws.close()
                 closesocket()
@@ -148,41 +148,41 @@ setPairUsdtData(JSON.parse(event.data))
                     <div style={{backgroundColor:"grey",padding:"10px"}}>{on?`CLOSE`:`CLOSE NOW`}</div>
             </button>
         {/* BUTTON WITH DIV */}
-            <button style={{  all: "unset",padding:"20px", cursor: "pointer"}}onClick={()=>{
+            <button style={{  all: "unset", cursor: "pointer"}}onClick={()=>{
 
           
                 }}>
 
                     <div style={{backgroundColor:"grey",padding:"10px"}}>Test</div>
             </button>
-    </nav>
+    </div>
     <div className='liveDataContainer'>
 
         
         {/* MAJOR  */}
         <div className='symbolDiv' style={{height:"10vh" ,marginBottom:"3vh"}}>
-            <text className='symbolText' style={{fontSize:"30px"}}>COIN</text>
+            <text className='symbolText' style={{fontSize:"3vw"}}>COIN</text>
         </div>
         <div className='priceDiv' style={{height:"10vh",marginBottom:"3vh"}}>
-            <text className='priceText' style={{fontSize:"30px",color:'#484848'}}>PRICE</text>
+            <text className='priceText' style={{fontSize:"3vw",color:'#484848'}}>PRICE</text>
         </div>
         <div className='profitDiv' style={{height:"10vh",marginBottom:"3vh"}}>
-            <text className='priceText' style={{fontSize:"30px"}}>PROFIT</text>
+            <text className='priceText' style={{fontSize:"3vw"}}>PROFIT</text>
         </div>
 
         {/* MAJOR DATA */}
         <div className='symbolDiv'>
-            <text className='symbolText' >{symbol}</text>
+            <text className='symbolText' style={{fontSize:"4vw"}} >{symbol}</text>
         </div>
         <div className='priceDiv'>
-            <text className='priceText' >$ {Math.round(usdtData.a*100000)/100000} </text>
+            <text className='priceText' style={{fontSize:"3vw"}} >$ {Math.round(usdtData.a*100000)/100000} </text>
         </div>
         <div className='profitDiv'>
             {/* <text className='priceText'  >{profit>0?`+ ${profit}`:profit}%</text> */}
             <div className='profitContainer'>
             <div style={{margin:"10px", padding:"10px" ,borderRight:"solid",borderColor:"white"}} >
-                <h4 style={{color:"white"}}>Zero Risk</h4>
-                <text className='priceText'  style={{fontSize:"30px"}} >{
+                <h4 style={{color:"white",fontSize:"1vw"}}>Zero Risk</h4>
+                <text className='priceText'  style={{fontSize:"2vw"}} >{
                     ( 
                         //Zero Risk Trade [take best in asks and bids]
                         Math.round(((100/usdtData.a)*(btcData.b)*(pairUsdtData.b)-100)*100)/100
@@ -191,8 +191,8 @@ setPairUsdtData(JSON.parse(event.data))
                     <br/>
             </div>
             <div  style={{margin:"10px"}}>
-                <h4 style={{color:"white",fontSize:"17px"}}>Risky</h4>
-                <text className='priceText'  style={{fontSize:"17px"}} >{
+                <h4 style={{color:"white",fontSize:"1vw"}}>Risky</h4>
+                <text className='priceText'  style={{fontSize:"1vw"}} >{
                     (
                         // Risky Trade [take best in asks and bids]
                         Math.round(((100/usdtData.b)*(btcData.a)*(pairUsdtData.a)-100)*100)/100
@@ -207,35 +207,35 @@ setPairUsdtData(JSON.parse(event.data))
 
         {/* PAIRS HEADING */}
         <div className='symbolDiv' style={{height:"10vh" ,marginTop:"5vh",marginBottom:"3vh"}}>
-            <text className='symbolText' style={{fontSize:"20px"}}>BUY {symbol}/USDT</text>
+            <text className='symbolText' style={{fontSize:"1.5vw"}}>BUY {symbol}/USDT</text>
         </div>
         <div className='priceDiv' style={{height:"10vh" ,marginTop:"5vh",marginBottom:"3vh"}}>
-            <text className='priceText' style={{fontSize:"20px"}}>Sell {symbol}/{pair}</text>
+            <text className='priceText' style={{fontSize:"1.5vw"}}>Sell {symbol}/{pair}</text>
         </div>
         <div className='profitDiv' style={{height:"10vh" ,marginTop:"5vh",marginBottom:"3vh"}}>
-            <text className='priceText' style={{fontSize:"20px"}}>SELL {pair}/USDT</text>
+            <text className='priceText' style={{fontSize:"1.5vw"}}>SELL {pair}/USDT</text>
         </div>
 
         {/* PAIRS DATA */}
         <div className='symbolDiv'>
             <div className='bidaskContainer'>
 
-            <text className='priceText' style={{fontSize:"32px",color:"#484848"}}>Asks : {usdtData.a}</text>
-            <text className='priceText' style={{fontSize:"23px" ,color:"#484848"}}>Bids : {usdtData.b}</text>
+            <text className='priceText' style={{fontSize:"2vw",color:"#484848"}}>Asks : {usdtData.a}</text>
+            <text className='priceText' style={{fontSize:"1.5vw" ,color:"#484848"}}>Bids : {usdtData.b}</text>
             </div>
         </div>
         <div className='priceDiv'>
             <div className='bidaskContainer'>
 
-            <text className='priceText' style={{fontSize:"23px"}}>Asks : {btcData.a}</text>
-            <text className='priceText' style={{fontSize:"32px"}}>Bids : {btcData.b}</text>
+            <text className='priceText' style={{fontSize:"2vw"}}>Asks : {btcData.a}</text>
+            <text className='priceText' style={{fontSize:"1.5vw"}}>Bids : {btcData.b}</text>
             </div>
         </div>
         <div className='profitDiv'>
         <div className='bidaskContainer'>
 
-                <text className='priceText' style={{fontSize:"23px"}}>Asks : {pairUsdtData.a}</text>
-                <text className='priceText' style={{fontSize:"32px"}}>Bids : {pairUsdtData.b}</text>
+                <text className='priceText' style={{fontSize:"1.5vw"}}>Asks : {pairUsdtData.a}</text>
+                <text className='priceText' style={{fontSize:"2vw"}}>Bids : {pairUsdtData.b}</text>
                 </div>
         </div>
 
